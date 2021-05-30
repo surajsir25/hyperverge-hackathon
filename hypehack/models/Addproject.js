40f066d4aaea60790c47d91e14a8b0f8ba7b6b79
iwpvnitsuaj <spsurajkumar.sp@gmail.com>
2021-05-30 14:41:53 +0530

    apis

---
+++ hypehack/models/Addproject.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    user: {
+        type:Schema.Types.ObjectId,
+        ref:'learner'
+    },
+    project_name: {
+        type: String,
+        required: true
+    },
+    tech_skill:{
+        type:String
+    },
+    project_description:{
+        type: String
+    },
+    feedback:{
+        type:String
+    },
+    project_rating:{
+        type:String
+    }
+});
+
+module.exports = Projectt = mongoose.model('project',PostSchema);
